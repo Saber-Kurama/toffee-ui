eadc2d10bc4380c41fefc5a210ed1c4d4e6b37bb
guochao <463561248@qq.com>
2021-05-27 20:05:44 +0800

    简单的调整

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,25 +1,4 @@
-const options = {
-  extraBabelPlugins: [
-      ['babel-plugin-import', {
-          libraryName: 'antd',
-          libraryDirectory: 'es',
-          style: true,
-      }]
-  ],
-  // cssModules: true,
-  extractCSS: true,
-  lessInBabelMode: true,
-  runtimeHelpers: true,
-  esm: 'babel', 
-  cjs: 'babel',
-  autoprefixer: {
-      browsers: [
-        "last 2 versions",
-        "Firefox ESR",
-        "> 1%",
-        "ie >= 11"
-      ],
-  }
+export default {
+  esm: 'rollup',
+  cjs: 'rollup',
 };
-
-export default options;
---
+++ asd.ts
@@ -0,0 +1,23 @@
+const options = {
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+      },
+    ],
+  ],
+  // cssModules: true,
+  extractCSS: true,
+  lessInBabelMode: true,
+  runtimeHelpers: true,
+  esm: 'babel',
+  cjs: 'babel',
+  autoprefixer: {
+    browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 11'],
+  },
+};
+
+export default options;
